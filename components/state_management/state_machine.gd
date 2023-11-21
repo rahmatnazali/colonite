758386ee64896df3a6b4813b6f0a357897666c62
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-21 11:07:03 +0700

    Add `enabled` on GenericStateMachine to alter the state activation

--- components/state_management/state_machine.gd
+++ components/state_management/state_machine.gd
@@ -2,6 +2,7 @@ extends Node
 class_name GenericStateMachine
 
 
+@export var enabled: bool = true
 @export var initial_state: GenericState
 
 
@@ -25,12 +26,12 @@ func _ready():
 
 
 func _process(delta):
-	if current_state != null:
+	if enabled and current_state != null:
 		current_state.update(delta)
 
 
 func _physics_process(delta):
-	if current_state != null:
+	if enabled and current_state != null:
 		current_state.physics_update(delta)
 
 
