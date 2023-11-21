502f19f15d746b7ef3ba9cf01e28fd028dbe585b
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-21 13:53:45 +0700

    trivial reformat on base_unit_2_movement.gd

--- debug/base_unit_2_movement.gd
+++ debug/base_unit_2_movement.gd
@@ -4,6 +4,7 @@ extends Control
 func _ready():
 	$BaseUnitCircular.disable_state()
 
+
 func _physics_process(delta):
 	# $BaseUnitCircular is always spinning to demonstrate that BaseUnit is only
 	# moving at where the head is pointed at
