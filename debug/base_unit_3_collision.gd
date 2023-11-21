0a79b455b7a857f75f24042b491ae659114f3ad0
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-21 14:48:25 +0700

    Adjust BaseUnit speed for base_unit_3_collision.tscn

--- debug/base_unit_3_collision.gd
+++ debug/base_unit_3_collision.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _ready():
-	$BaseUnit.disable_state()
+	$BaseUnit1.disable_state()
 	$BaseUnit2.disable_state()
 	$BaseUnit3.disable_state()
 	$BaseUnit4.disable_state()
 	
-	$Node2D/BaseUnit.disable_state()
-	$Node2D/Enemy.disable_state()
-	$Node2D/Enemy2.disable_state()
-	$Node2D/Enemy3.disable_state()
+	$SmallEnemy1.disable_state()
+	$SmallEnemy2.disable_state()
+	$SmallEnemy3.disable_state()
+	$SmallEnemy4.disable_state()
