0e0b8198b9ab6e654756a313331543ee4f3eac0a
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-22 09:16:08 +0700

    Fix strange collision behavior on base_unit_3_collision.tscn by instantiating another new BaseUnit

--- debug/base_unit_3_collision.gd
+++ debug/base_unit_3_collision.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 func _ready():
-	$BaseUnit1.disable_state()
-	$BaseUnit2.disable_state()
-	$BaseUnit3.disable_state()
-	$BaseUnit4.disable_state()
-	
 	$SmallEnemy1.disable_state()
 	$SmallEnemy2.disable_state()
 	$SmallEnemy3.disable_state()
 	$SmallEnemy4.disable_state()
+	
+	$MiddleUnit1.disable_state()
+	$MiddleUnit2.disable_state()
+	$MiddleUnit3.disable_state()
+	$MiddleUnit4.disable_state()
