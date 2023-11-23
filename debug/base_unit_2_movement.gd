c47538a95f9d21ea253cbdee33b7f4e759dcc67a
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-23 10:29:35 +0700

    Remove BaseUnitCircular on base_unit_2_movement.tscn

--- debug/base_unit_2_movement.gd
+++ debug/base_unit_2_movement.gd
@@ -2,10 +2,8 @@ extends Control
 
 
 func _ready():
-	$BaseUnitCircular.disable_state()
+	pass
 
 
 func _physics_process(delta):
-	# $BaseUnitCircular is always spinning to demonstrate that BaseUnit is only
-	# moving at where the head is pointed at
-	$BaseUnitCircular.rotation += 10 * delta
+	pass
