df4c496aefc8691f1b4dd9ba6610ec366569e3c6
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-23 16:43:59 +0700

    Add attack_range on BaseUnit Chase state, and transition to attack state when target_unit is within attack_range

--- components/unit/base_unit/base_unit_state_chase.gd
+++ components/unit/base_unit/base_unit_state_chase.gd
@@ -3,6 +3,7 @@ class_name BaseUnitChase
 
 
 @export var character: BaseUnit
+@export var attack_range: Area2D
 
 
 var target_unit: BaseUnit
@@ -11,6 +12,7 @@ var target_unit: BaseUnit
 func enter(payload = null):
 	assert(character != null, 'ChaseState: Character is not provided (null)')
 	assert(payload is BaseUnit, 'ChaseState: Provided state payload is not BaseUnit')
+	assert(attack_range is Area2D, 'ChaseState: Provided attack_range is not Area2D')
 
 	target_unit = payload
 	character.current_speed = character.speed_chase
@@ -24,6 +26,11 @@ func physics_update(_delta):
 		# move towards target_unit
 		var direction_to_enemy = (target_unit.global_position - character.global_position).normalized()
 		character.move_towards_direction(direction_to_enemy)
+		
+		# if target_unit is within the attack range, transition to attack state
+		if target_unit in attack_range.get_overlapping_bodies():
+			transitioned.emit(self, 'attack', target_unit)
+		
 	else:
 		# back to wandering
 		transitioned.emit(self, 'wander')
