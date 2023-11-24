9cbd30f34c4abd1096bb4b39eade7cbd085c6585
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-24 08:14:15 +0700

    Add BaseUnit Attack state

---
+++ components/unit/base_unit/base_unit_state_attack.gd
@@ -0,0 +1,41 @@
+extends GenericState
+class_name BaseUnitAttack
+
+
+@export var character: BaseUnit
+@export var attack_range: Area2D
+@export var consume_range: Area2D
+
+
+var target_unit: BaseUnit
+
+
+func enter(payload = null):
+	assert(character != null, 'ChaseState: Character is not provided (null)')
+	assert(payload is BaseUnit, 'ChaseState: Provided state payload is not BaseUnit')
+	assert(attack_range is Area2D, 'ChaseState: Provided attack_range is not Area2D')
+
+	target_unit = payload
+	character.current_speed = character.speed_attacking
+
+	if character.state_verbose_mode: 
+		print(character.name, ': entering attack state with payload: ', payload)
+
+
+func physics_update(_delta):
+	if target_unit.is_alive and target_unit in attack_range.get_overlapping_bodies():
+		if character.attack_component.able_to_attack():
+			var is_attack_succeed = character.attack_component.attack()
+			if is_attack_succeed:
+				target_unit.health_component.take_damage(character.attack_component.get_damage())
+	elif target_unit.is_alive:
+		# alive but not in range
+		transitioned.emit(self, 'chase', target_unit)
+	elif target_unit.is_alive == false and target_unit in consume_range.get_overlapping_bodies():
+		# todo
+		# should attempt to consume
+		print('attempt to consume')
+	else:
+		# back to wandering
+		transitioned.emit(self, 'wander')
+
