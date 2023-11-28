15a8eb7fc36ed6284657a4eded79fbb6d8c932f9
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-11-28 09:28:50 +0700

    Add GlobalStatsManager to manage resource and the update between it

---
+++ globals/global_stats_manager.gd
@@ -0,0 +1,34 @@
+extends Node
+
+
+signal resource_changed
+
+
+var resource: int = 0: set = set_resource, get = get_resource
+var base_unit_cost: int = 50
+
+
+func set_resource(new_resource):
+	resource = new_resource
+	emit_signal('resource_changed')
+
+
+func get_resource():
+	return resource
+
+
+func consume_resource(resource_amount: int) -> bool:
+	if resource_amount <= resource:
+		set_resource(resource - resource_amount)
+		return true
+	return false
+
+
+func purchase_base_unit() -> bool:
+	var has_enough_resource = consume_resource(base_unit_cost)
+	return has_enough_resource
+
+
+func base_unit_consumed():
+	set_resource(resource + base_unit_cost * 4)
+
