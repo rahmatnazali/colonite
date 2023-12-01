1ea11a118b2ad20f83f2bccb211664c2e7696152
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-12-01 15:06:18 +0700

    Add close on esc

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -42,3 +42,10 @@ func _on_how_to_play_button_pressed():
 func _on_setting_button_pressed():
 	get_tree().change_scene_to_file("res://menu/settings.tscn")
 
+
+# close on ESC
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
+
